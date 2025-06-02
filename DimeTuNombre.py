7a64051b80be61272a7b7dc74282f8cbf52ec9c3
Benjamin Ferrera <be.ferrera@duocuc.cl>
2025-06-01 21:22:59 -0400

    Dime tu Nombre

---
+++ DimeTuNombre.py
@@ -0,0 +1,12 @@
+FUNCION: PEDIR NOMBRE
+
+nombre  = input("Hola, dime tu nombre: ")
+edad = int(input("Dime tu edad: "))
+
+print(f"Hola {nombre}, tienes {edad} años.")
+
+#1.Nombre: La linea 1 del codigo es asignar variable "nombre" con la accion al usuiario que seria input. Es para mostrar mensaje en pantalla, acompañado del mensaje. Todo lo que ponga el usuario se almacena en la variable "nombre".
+
+#2.Edad: La linea 2 del codigo es asignar variable "edad" con la accion al usuario que seria en este caso "int(input", ahora se agrega int que seria para que el usuario pueda guardar numeros en la respuesta, acompañado del mensaje que le pide la edad. Igualmente todo lo que ponga el usuario se almacena pero en este caso seria en la variable "edad".
+
+#3.El proposito de la ultima linea de codigo seria "llamar" a los datos que el usuario ingreso.La "f" es para poder "invocar" multiples variables a una cadena (que es string en ingles uwu) de texto. Luego uno ordena con los datos que quiere llamar en el orden logico con {variable} donde vayan, con el msj que tenga la logica igual de lo que estamos haciendo. 
