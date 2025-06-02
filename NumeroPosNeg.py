ba791f44f1688dcb595d670e1daa5311266fee6c
Benjamin Ferrera <be.ferrera@duocuc.cl>
2025-06-01 22:03:33 -0400

    Add files via upload

---
+++ NumeroPosNeg.py
@@ -0,0 +1,13 @@
+VERIFICAR NUMERO POSITIVO O NEGATIVO.
+
+variable1 = int(input("ingresa un numero: "))
+
+if variable1 < 0:    
+    print("tu numero es negativo")
+elif variable1 > 0:   
+    print("tu numero es positivo")
+else:   
+    print("tu numero es igual cero") 
+
+
+
