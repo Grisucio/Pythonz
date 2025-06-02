ed29dc5f61506b54ff7acf8ffb271414739bbde2
Benjamin Ferrera <be.ferrera@duocuc.cl>
2025-06-01 21:21:40 -0400

    Update EdadyNotas.py

--- EdadyNotas.py
+++ EdadyNotas.py
@@ -23,18 +23,18 @@ nota = float(input("Dime tu nota: "))
 
 #EDAD
 #1.SOLICITAR EDAD.
-   la primera linea de codigo simplemente le pide al usuario su edad. Agregando el int para habilitar al usuario el uso de numeros.
+   #la primera linea de codigo simplemente le pide al usuario su edad. Agregando el int para habilitar al usuario el uso de numeros.
 
 #2.CONDICIONES.
-   La segunda parte del codigo nos introduce a las condiciones, que en este caso la primera seria: si la edad es mayor o igual a 18 se le muestra al usuario que es mayor de edad.
-y la segunda: si no, mostrar al usuario que es menor de edad.
+   #La segunda parte del codigo nos introduce a las condiciones, que en este caso la primera seria: si la edad es mayor o igual a 18 se le muestra al usuario que es mayor de edad.
+   #y la segunda: si no, mostrar al usuario que es menor de edad.
 
 #NOTAS
 #1.SOLICITAR NOTA:
-   La primera parte del codigo nos pide que el usuario indique su nota, usamos el float ademas del input para que se puedan poner decimales al numero,se indican con un . no con una ,
+   #La primera parte del codigo nos pide que el usuario indique su nota, usamos el float ademas del input para que se puedan poner decimales al numero,se indican con un . no con una ,
 
 #2.CONDIOCIONES:
-   La segunda parte del codigo es aplicar las condiciones solicitadas segun correspondan con if, elif y else.
+   #La segunda parte del codigo es aplicar las condiciones solicitadas segun correspondan con if, elif y else.
 
 
 
